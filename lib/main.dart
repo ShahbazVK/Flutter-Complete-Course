b1951f91b01559bbbbed3449ccaaef5e12817741
Shahbaz Ali <shahbazalivk@gmail.com>
2022-04-11 14:08:46 +0500

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_catalog/pages/home_page.dart';
+import 'package:flutter_project_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    // var day = "Tuesday";
+    // const pi = 3.142;
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome To 30 days of Flutter"),
-            ),
-        )
+      // home: HomePage(),
+      themeMode:ThemeMode.light,//type dark remove light
+      theme: ThemeData(primarySwatch:Colors.green),
+      darkTheme: ThemeData (
+        brightness: Brightness.dark,
       ),
+      initialRoute: "/home",
+      routes: {
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage(),
+      },//map in flutter, dictionary in python, json in js, hover over route
     );
   }
 }
