718b7fc6416e4551158ee4979ad6856a8672c883
Shahbaz Ali <shahbazalivk@gmail.com>
2022-04-23 04:42:44 +0500

    day 9 completed

---
+++ lib/models/catalogue.dart
@@ -0,0 +1,14 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+//curly braces ka scene
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final products = [
+  Item("codepur0", "Iphone 12 pro", "4th generation", 999, "#33505a", "image")
+];
